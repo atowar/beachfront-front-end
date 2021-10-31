6e251ac504f17086445231dced20f1e980d140de
Atowar Rahman <ahmed.anik76@gmail.com>
2021-10-31 22:45:33 +0600

    updated

--- src/Components/Pages/Home/AboutSection/AboutSection.js
+++ src/Components/Pages/Home/AboutSection/AboutSection.js
@@ -5,8 +5,8 @@ const AboutSection = () => {
     return (
         <div className="about-section grid justify-items-center">
             <div className="pt-10 text-white">
-
-
+            <h2 className="md:text-5xl text-xl p-3">Planning a brief or extended stay at Cox’s Bazar?</h2>
+                <h3 className="text-xl md:text-4xl p-3">The rooms and suites at Long Beach Hotel remarkably fulfills style, comfort and surrounding.</h3>
             </div>
         </div>
     );
